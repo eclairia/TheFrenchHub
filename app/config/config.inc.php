65bd447455f10551a7cc7b968ac851939552350c
Guillaume <guillaumehulin93@gmail.com>
2017-03-02 18:37:36 +0100

    Ajout d'un niveau de user dans config

--- app/config/config.inc.php
+++ app/config/config.inc.php
@@ -3,12 +3,12 @@
 	define("PAGE_LANG", "fr");
 	define("PAGE_CHARSET", "utf-8");
 
-	define("DEFAULT_MODULE", "article");
-	define("DEFAULT_ACTION", "index");
+	define("DEFAULT_MODULE", "project");
+	define("DEFAULT_ACTION", "list");
 
 	define("PAGINATION_ARTICLES", 3);
-	define("PAGINATION_COMMENTAIRES", 5);
 
+    define("USER_SUPER_ADMIN", 2);
 	define("USER_ADMIN", 1);
 	define("USER_LAMBDA", 0);
 
