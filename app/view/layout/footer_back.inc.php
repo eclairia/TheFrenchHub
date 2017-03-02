dacdf486e2de5bf242cd765c2b5f424f3a21fd20
Guillaume <guillaumehulin93@gmail.com>
2017-03-03 00:35:09 +0100

    Création des footer et header back de référence

---
+++ app/view/layout/footer_back.inc.php
@@ -0,0 +1,3 @@
+		<script src="webroot/JS/bootstrap.min.js"></script>
+	</body>
+</html>
---
+++ app/view/layout/header_back.inc.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="<?= PAGE_LANG ?>">
+<head>
+	<meta charset="<?= PAGE_CHARSET ?>">
+	<title><?= PAGE_TITLE ?></title>
+	<link rel='stylesheet' type="text/css" href='webroot/CSS/bootstrap.min.css' />
+	<link rel='stylesheet' type="text/css" href='webroot/CSS/style.css' />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+</head>
+<body>
+
+	<div class="container-fluid">
+		<header>
+			<nav>
+				<ul class="nav nav-pills">
+					<li role="presentation" class="active"><a href="?module=projects&action=list">Liste des projets</a></li>
+					<li role="presentation"><a href="?module=users&action=list">Liste des utilisateurs</a></li>
+					<li role="presentation"><a href="?module=services&action=list">Liste des offres</a></li>
+					<li role="presentation"><a href="?module=articles&action=list">Liste des articles</a></li>
+				</ul>
+			</nav>
+		</header>
+	</div>
+
+
+
+
