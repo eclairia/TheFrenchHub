1e8512421fd754f1da55a333616e1be6fd2779ef
Guillaume <guillaumehulin93@gmail.com>
2017-03-04 00:52:46 +0100

    Correction des liens de redirection et jQuery externalisé

--- app/view/projects/list.php
+++ app/view/projects/list.php
@@ -154,39 +154,6 @@
         </div>
     </div>
 
-    <script type="text/javascript">
-        function accepter (id) {
-            $('#confirmModal').on('show.bs.modal', function (event) {
-                var confirm = $('#confirm');
-                var button = $(event.confirm); // Button that triggered the modal
-                var recipient = button.data('whatever') // Extract info from data-* attributes
-                // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-                // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-                var modal = $(this);
-                modal.find('.modal-title').text('New message to ' + recipient);
-                modal.find('.modal-body input').val(recipient);
-            });
-            $('#validerProjet').click(function () {
-                $(location).attr('href','?module=projects&action=list&id='+ id +'&status=accepted');
-            });
-        }
-
-
-        function refuser (id) {
-            $('#refuseModal').on('show.bs.modal', function (event) {
-                var confirm = $('#refuse');
-                var button = $(event.confirm); // Button that triggered the modal
-                var recipient = button.data('whatever') // Extract info from data-* attributes
-                // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-                // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-                var modal = $(this);
-                modal.find('.modal-title').text('New message to ' + recipient);
-                modal.find('.modal-body input').val(recipient);
-            });
-            $('#refuserProjet').click(function () {
-                $(location).attr('href','?module=projects&action=list&id='+ id +'&status=accepted');
-            });
-        }
-    </script>
+    <script type="text/javascript" src="webroot/JS/status_project.js"></script>
 <?php
     include_once "app/view/layout/footer_back.inc.php";
