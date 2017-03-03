2c684e5bec1fac13f0f20ec44f16ab9bf834170a
Guillaume <guillaumehulin93@gmail.com>
2017-03-03 12:27:31 +0100

    affichage dynamique du hover sur les boutons du menu

--- app/view/layout/header_back.inc.php
+++ app/view/layout/header_back.inc.php
@@ -8,15 +8,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 </head>
 <body>
-
 	<div class="container-fluid">
 		<header>
 			<nav>
 				<ul class="nav nav-pills">
-					<li role="presentation" class="active"><a href="?module=projects&action=list">Liste des projets</a></li>
-					<li role="presentation"><a href="?module=users&action=list">Liste des utilisateurs</a></li>
-					<li role="presentation"><a href="?module=services&action=list">Liste des offres</a></li>
-					<li role="presentation"><a href="?module=articles&action=list">Liste des articles</a></li>
+					<li role="presentation" <?php if($_GET['module'] == 'projects'){echo 'class="active"';} ?> ><a href="?module=projects&action=list">Liste des projets</a></li>
+					<li role="presentation" <?php if($_GET['module'] == 'users'){echo 'class="active"';} ?> ><a href="?module=users&action=list">Liste des utilisateurs</a></li>
+					<li role="presentation" <?php if($_GET['module'] == 'services'){echo 'class="active"';} ?> ><a href="?module=services&action=list">Liste des offres</a></li>
+					<li role="presentation" <?php if($_GET['module'] == 'articles'){echo 'class="active"';} ?> ><a href="?module=articles&action=list">Liste des articles</a></li>
 				</ul>
 			</nav>
 		</header>
