c40eab8dbc3d6602d8d5f5fe28d16cc16012de1d
Romain Rouleau <Romain Rouleau>
2017-03-10 18:07:42 +0100

    modif home + dossier img

--- app/view/static/home.php
+++ app/view/static/home.php
@@ -1,10 +1,37 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
+	<head>
+		<title></title>
+		<meta charset="utf-8"/>
+		<link rel='stylesheet' type="text/css" href='../webroot/CSS/style.css' />
+	</head>
+
 <body>
 
+	<nav>
+		<a href="../home.php">
+	  	<div id="navleft">
 
-</body>
+				<div id="logotype"></div>
+				<div id="txtlogo"></div>
+
+			</div>
+		</a>
+
+		<div id="navright">
+
+			<div><a href="#">Le concept</a></div>
+			<div><a href="#">Nos offres</a></div>
+			<div><a href="#">Actualités</a></div>
+			<div><a href="#">L'équipe</a></div>
+			<div><a href="#">Connexion</a></div>
+			<div><a href="#">Inscription</a></div>
+
+		</div>
+	</nav>
+
+	
+
+	</body>
+
 </html>
