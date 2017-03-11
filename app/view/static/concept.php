1535c26cd73d6415796558856be05dea2632f3ee
Romain Rouleau <Romain Rouleau>
2017-03-12 00:23:11 +0100

    denières modifs page concept

--- app/view/static/concept.php
+++ app/view/static/concept.php
@@ -7,13 +7,24 @@
 			<div id="conceptrelative">
 
 			  <div id="textconceptleft">
+
 					<h1 id="titleconcept">Le concept</h1>
-					<p id="txtconcept">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+
+					<div id="twopara">
+
+					<p class="txtconcept botspace">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+					<p class="txtconcept"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 						 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+					</div>
+
 				</div>
 
 				<div>
+
 					<img id="logoanim" src="webroot/IMG/logo3d.png"/>
+
 				</div>
 
 			</div>
