f938b75f536b99300aa3ea166c8cac4ea8a8eadb
Romain Rouleau <Romain Rouleau>
2017-03-12 00:08:11 +0100

    page concept

---
+++ app/controller/static/concept.php
@@ -0,0 +1,6 @@
+<?php
+
+
+	define("APP_LANG", "fr");
+	define("PAGE_TITLE", 'Présentation du concept The French Hub');
+	include_once('app/view/static/concept.php');
---
+++ app/view/static/concept.php
@@ -0,0 +1,42 @@
+	<?php include_once 'app/view/layout/header_front.inc.php'; ?>
+
+	<div id="contentconcept">
+
+		<div id="headerconcept">
+
+			<div id="conceptrelative">
+
+			  <div id="textconceptleft">
+					<h1 id="titleconcept">Le concept</h1>
+					<p id="txtconcept">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+						 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+
+				<div>
+					<img id="logoanim" src="webroot/IMG/logo3d.png"/>
+				</div>
+
+			</div>
+
+		</div>
+
+
+		<div id="othercontentconcept">
+
+			<div id="bgconcept1"><div class="fd"></div></div>
+
+			<div id="conceptexplain1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/><br/> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br/><br/> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+				 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+
+			<div id="bgconcept2"></div>
+
+			<div id="conceptexplain2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/><br/> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br/><br/> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+				 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+
+			<div id="bgconcept3"><div class="fd"></div></div>
+
+		</div>
+
+	</div>
+	<!-- Footer -->
+	<?php include_once 'app/view/layout/footer_front.inc.php'; ?>
