627ed612794ffec7ffd00cc6c0a1238f6fe6fbdf
Romain Rouleau <Romain Rouleau>
2017-03-11 22:13:05 +0100

    page equipe

---
+++ app/view/static/equipe.php
@@ -0,0 +1,70 @@
+	<?php include_once 'app/view/layout/header_front.inc.php'; ?>
+
+	<div id="contentequipe">
+
+		  <div id="backgroundequipe">
+
+				  <div id="blackteam">
+
+						<div id="rotatetitle">
+
+						  <h1 id="titreequipe">Les Fondateurs</h1>
+
+						</div>
+
+					</div>
+
+			</div>
+
+			<div id="membres">
+
+				<div id="teamhaut">
+
+					<div id="amel" class="divmember">
+						<div class="teamdisplaynone">
+							<p class="textnone">Amel Guellal <br/><span class="poles">Pôle Marketing</span></p>
+						</div>
+					</div>
+
+					<div id="guillaume" class="divmember">
+						 <div class="teamdisplaynone">
+							 <p class="textnone">Guillaume Hulin <br/><span class="poles">Pôle Développement</span></p>
+						 </div>
+					 </div>
+
+					<div id="carla" class="divmember">
+						 <div class="teamdisplaynone">
+							 <p class="textnone">Carla Succar <br/><span class="poles">Pôle Webdesign</span></p>
+						 </div>
+					 </div>
+
+				</div>
+
+				<div id="teambas">
+
+					<div id="louis" class="divmember">
+						<div class="teamdisplaynone">
+							<p class="textnone">Louis FIessinger <br/><span class="poles">Pôle Marketing</span></p>
+						</div>
+					</div>
+
+					<div id="adrien" class="divmember">
+						<div class="teamdisplaynone">
+							<p class="textnone">Adrien Jourdier <br/><span class="poles">Pôle Développement</span></p>
+						</div>
+				 </div>
+
+					<div id="romain" class="divmember">
+						<div class="teamdisplaynone">
+							<p class="textnone">Romain Rouleau <br/><span class="poles">Pôle Webdesign</span></p>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+
+	</div>
+
+	<!-- Footer -->
+	<?php include_once 'app/view/layout/footer_front.inc.php'; ?>
