f761e35fa36c9bfa916a9841e9f14c8fb83e9902
Adrien <you@example.com>
2017-03-12 20:46:29 +0100

    Connexion users operationnel

--- app/controller/users/login.php
+++ app/controller/users/login.php
@@ -1,5 +1,28 @@
 <?php
 
-define("APP_LANG", "fr");
-define("PAGE_TITLE", "Connectez-vous à votre compte The French Hub");
-include_once("app/view/users/login.php");
+	if(!isset($_POST["user_login"]))
+	{
+		define("APP_LANG", "fr");
+		define("PAGE_TITLE", "Connectez vous à votre compte the French Hub");
+		include_once("app/view/users/login.php");
+	}
+
+	else
+	{
+		$_POST["user_password"] = md5($_POST["user_password"] . SALT);
+
+		//Appel du modèle pour chercher un user
+		include_once("app/model/users/verif_login.php");
+		$retour = verif_login($_POST);
+
+		if(!$retour)
+		{
+			location("users", "login", "notif=nok");
+		}
+
+		else
+		{
+			$_SESSION["user"] = $retour;
+			location("projects", "index", "notif=ok");
+		}
+	}
