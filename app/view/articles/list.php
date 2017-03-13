387f63028e1b60e31744ab63dc87259f6f699c2a
Adrien <you@example.com>
2017-03-13 12:27:10 +0100

    Suppresion d'un article fonctionnel

--- app/view/articles/list.php
+++ app/view/articles/list.php
@@ -1,11 +1,11 @@
 <?php
-	// var_dump($articles);
 	foreach($articles as $article)
 	{ ?>
+		<img src="<?= 'webroot/IMG/' . $article["article_picture_url"]; ?>">
 		<p><?= $article["article_title"]; ?></p>
 		<p><?= $article["article_id"]; ?></p>
 		<p><?= $article["article_content"]; ?></p>
 		<p><?= $article["article_date"]; ?></p>
 		<a href="?module=articles&action=update&id=<?= $article['article_id']; ?>">Modifier le contenu de l'article</a><br>
-		<a href="?module=articles&action=delete&id=<?= $article['article_id']; ?>">Supprimer l'article</a>
+		<a href="?module=articles&action=delete&id=<?= $article['article_id']; ?>">Supprimer l'article</a><br><br>
 	<?php } ?>
