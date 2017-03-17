392f26807ba28e7cb2ebbe15397236a5f695ceac
Guillaume <guillaumehulin93@gmail.com>
2017-03-17 11:25:32 +0100

    Ajout fonction select_table_value dans le core model

--- core/coremodel.php
+++ core/coremodel.php
@@ -156,6 +156,79 @@
         }
     }
 
+    function select_table_value($table, $options=array(), $and=array())
+    {
+        global $pdo;
+
+        /*$nb_table = count($table);
+        echo $nb_table;
+        exit;*/
+
+        try
+        {
+            $sql = "SELECT * FROM ";
+
+            $i = 1;
+            while ($i <= count($table))
+            {
+                $sql .= " " . $table["table".$i];
+                if ((count($table) > 1) && ($i != count($table)))
+                {
+                    $sql .= ",";
+                }
+                $i++;
+            }
+
+            if ((isset($options["where_column"])) && (isset($options["where_value"])))
+            {
+                $sql .= " WHERE " . $options["where_column"] . " = '" . $options["where_value"] . "'";
+            }
+
+            if ((isset($and)) && (isset($and)))
+            {
+                $i = 1;
+                while ($i <= count($and)/2 )
+                {
+                    $sql .= " AND " . $and["and_column".$i] . " = '" . $and["and_value".$i] . "'";
+                    $i++;
+                }
+            }
+
+            if (isset($options["orderby"]))
+            {
+                $sql .= " ORDER BY " . $options["orderby"];
+            }
+
+            if (isset($options["order"]))
+            {
+                $sql .= " " . $options["order"];
+            }
+
+            if (isset($options["limit"]))
+            {
+                $sql .= " LIMIT ";
+                if (isset($options["offset"]))
+                {
+                    $sql .= $options["offset"] . ", ";
+                }
+                $sql .= $options["limit"];
+            }
+
+            //echo $sql; exit;
+
+            $query = $pdo->query($sql);
+            $data = $query->fetchAll();
+            $query->closeCursor();
+            // var_dump($query);
+            // die();
+            return $data;
+        }
+        catch (Exception $e)
+        {
+            die('Erreur SQL :' .$e->getMessage());
+        }
+    }
+
 	function deletetable($table, $options=array(), $and=array())
 	{
 		global $pdo;
@@ -197,7 +270,6 @@
 
 			return true;			
 		}
-
 		catch(Exception $e)
 		{
 		    echo $sql;
