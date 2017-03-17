d97d6d2d3ad751e681b23ed23b02df3191ebce7f
Romain Rouleau <Romain Rouleau>
2017-03-17 20:20:15 +0100

    page articles

---
+++ app/view/articles/feed.php
@@ -0,0 +1,193 @@
+<?php include_once 'app/view/layout/header_front.inc.php'; ?>
+
+<div id="allcontentarticles">
+
+<!-- ARTICLE 1 -->
+ <div class="articlel">
+
+	<div class="articlepartleft">
+
+		<div class="blocbleuarticle">
+			  <div><img src="webroot/IMG/idea.png" alt=""/></div>
+		</div>
+
+		<div class="underbloc1">
+			  <p>01/01/2017</p>
+				<div></div>
+		</div>
+
+	</div>
+
+	<div class="articlepartright">
+
+			<div class="righttop">
+
+					<div class="articlehr">
+
+					</div>
+
+					<div class="titreright">
+						<h2> Lorem Title de merde </h2>
+					</div>
+
+			</div>
+
+			<div class="txtarticle">
+				<p class="internaltxt">Sed quid est quod in hac causa maxime homines admirentur et reprehendant meum consilium,
+					cum ego idem antea multa decreverim, que magis ad hominis dignitatem quam ad rei publicae necessitatem pertinerent? Supplicationem quindecim dierum decrevi sententia mea.
+					 Rei publicae satis erat tot dierum quot C. Mario ; dis immortalibus non erat exigua eadem gratulatio quae ex maximis bellis.</p>
+			</div>
+
+			<div class="buttonarticle">
+				<a href="#" target="_blank">Voir</a>
+			</div>
+
+	</div>
+
+</div>
+
+<!-- FIN ARTICLE 1 -->
+
+<!-- ARTICLE 2 -->
+ <div class="article2">
+
+
+	<div class="articlepartright">
+
+			<div class="righttop2">
+
+					<div class="articlehr2">
+
+					</div>
+
+					<div class="titreright2">
+						<h2> Lorem Title de merde </h2>
+					</div>
+
+			</div>
+
+			<div class="txtarticle2">
+				<p class="internaltxt2">Sed quid est quod in hac causa maxime homines admirentur et reprehendant meum consilium,
+					cum ego idem antea multa decreverim, que magis ad hominis dignitatem quam ad rei publicae necessitatem pertinerent? Supplicationem quindecim dierum decrevi sententia mea.
+					 Rei publicae satis erat tot dierum quot C. Mario ; dis immortalibus non erat exigua eadem gratulatio quae ex maximis bellis.</p>
+			</div>
+
+			<div class="buttonarticle2">
+				<a href="#" target="_blank">Voir</a>
+			</div>
+
+	</div>
+
+	<div class="articlepartleft2">
+
+		<div class="blocbleuarticle2">
+			  <div><img src="webroot/IMG/idea.png" alt=""/></div>
+		</div>
+
+		<div class="underbloc2">
+			  <p>01/01/2017</p>
+				<div></div>
+		</div>
+
+	</div>
+
+</div>
+
+<!-- FIN ARTICLE 2 -->
+
+<!-- ARTICLE 3 -->
+ <div class="articlel">
+
+	<div class="articlepartleft">
+
+		<div class="blocbleuarticle">
+			  <div><img src="webroot/IMG/idea.png" alt=""/></div>
+		</div>
+
+		<div class="underbloc1">
+			  <p>01/01/2017</p>
+				<div></div>
+		</div>
+
+	</div>
+
+	<div class="articlepartright">
+
+			<div class="righttop">
+
+					<div class="articlehr">
+
+					</div>
+
+					<div class="titreright">
+						<h2> Lorem Title de merde </h2>
+					</div>
+
+			</div>
+
+			<div class="txtarticle">
+				<p class="internaltxt">Sed quid est quod in hac causa maxime homines admirentur et reprehendant meum consilium,
+					cum ego idem antea multa decreverim, que magis ad hominis dignitatem quam ad rei publicae necessitatem pertinerent? Supplicationem quindecim dierum decrevi sententia mea.
+					 Rei publicae satis erat tot dierum quot C. Mario ; dis immortalibus non erat exigua eadem gratulatio quae ex maximis bellis.</p>
+			</div>
+
+			<div class="buttonarticle">
+				<a href="#" target="_blank">Voir</a>
+			</div>
+
+	</div>
+
+</div>
+
+<!-- FIN ARTICLE 3 -->
+
+<!-- ARTICLE 4 -->
+ <div class="article2">
+
+
+	<div class="articlepartright">
+
+			<div class="righttop2">
+
+					<div class="articlehr2">
+
+					</div>
+
+					<div class="titreright2">
+						<h2> Lorem Title de merde </h2>
+					</div>
+
+			</div>
+
+			<div class="txtarticle2">
+				<p class="internaltxt2">Sed quid est quod in hac causa maxime homines admirentur et reprehendant meum consilium,
+					cum ego idem antea multa decreverim, que magis ad hominis dignitatem quam ad rei publicae necessitatem pertinerent? Supplicationem quindecim dierum decrevi sententia mea.
+					 Rei publicae satis erat tot dierum quot C. Mario ; dis immortalibus non erat exigua eadem gratulatio quae ex maximis bellis.</p>
+			</div>
+
+			<div class="buttonarticle2">
+				<a href="#" target="_blank">Voir</a>
+			</div>
+
+	</div>
+
+	<div class="articlepartleft2">
+
+		<div class="blocbleuarticle2">
+			  <div><img src="webroot/IMG/idea.png" alt=""/></div>
+		</div>
+
+		<div class="underbloc2">
+			  <p>01/01/2017</p>
+				<div></div>
+		</div>
+
+	</div>
+
+</div>
+
+<!-- FIN ARTICLE 4 -->
+
+ </div>
+
+<?php include_once 'app/view/layout/footer_front.inc.php'; ?>
