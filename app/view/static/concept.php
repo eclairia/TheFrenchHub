06d0b977e772d322de2a12e591ed7b7315fda5af
Romain Rouleau <Romain Rouleau>
2017-03-18 01:44:21 +0100

    page concept refonte

--- app/view/static/concept.php
+++ app/view/static/concept.php
@@ -2,53 +2,69 @@
 
 	<div id="contentconcept">
 
-		<div id="headerconcept">
+		<div id="conceptflex">
 
-			<div id="conceptrelative">
+			<div>
 
-			  <div id="textconceptleft">
-
-					<h1 id="titleconcept">Le concept</h1>
-
-					<div id="twopara">
-
-					<p class="txtconcept botspace">The French Hub redéfinit les enjeux de l’incubation.
-						Elle s’appuie sur des notions de partage, de transmissions et d’interconnectivité.
-						Ainsi, elle permet à de nombreux entrepreneurs de construire des projets novateurs.</p>
-
-					</div>
-
-				</div>
+				<h1 id="titleconcept">Le concept</h1>
 
 				<div>
 
-					<img id="logoanim" src="webroot/IMG/logo3d.png" alt=""/>
+				<p class="txtconcept botspace">The French Hub redéfinit les enjeux de l’incubation.
+					Elle s’appuie sur des notions de partage, de transmissions et d’interconnectivité.
+					Ainsi, elle permet à de nombreux entrepreneurs de construire des projets novateurs.</p>
 
 				</div>
 
 			</div>
 
-		</div>
-
-
-		<div id="othercontentconcept">
-
-			<div id="bgconcept1"><div class="fd"></div></div>
-
-			<div id="conceptexplain1">Chez the French Hub, il est question de réunir des individus de différentes horizons, de les faire travailler ensemble, et donc de faire émerger des solutions innovantes.
-Échanger dans un monde ou les barrières n’existent plus, voila l’avantage sur lequel s’appuie The French Hub. De la sorte, il alimente la notion d’interconnectivité et de transmission entre de jeunes entrepreneurs à travers le monde.
-</div>
-
-			<div id="bgconcept2"></div>
-
-			<div id="conceptexplain2">Grâce à nos partenariats avec des fonds et des entreprises qualifiés, nous mettons les entrepreneurs dans les meilleures dispositions pour réussir leurs projets.
-L’environnement de travail est primordiale, il est impératif de baigner dans un environnement dynamique et rassurant pour réussir dans les meilleures conditions.
-</div>
-
-			<div id="bgconcept3"><div class="fd"></div></div>
+			<div>
+				  <img id="logoanim" src="webroot/IMG/logo3d.png" alt=""/>
+			</div>
 
 		</div>
 
-	</div>
+		<div id="backgrdconcept">
+
+			<div id="ccpt">
+
+				<div id="ccptimg">
+
+					<div id="bgccpt1"></div>
+					<div id="bgccpt2"></div>
+					<div id="bgccpt3"></div>
+
+				</div>
+
+				<div>
+
+					<div id="conceptexplain1">Chez the French Hub, il est question de réunir des individus de différents horizons, de les faire travailler ensemble, et donc de faire émerger des solutions innovantes.
+						Échanger dans un monde où les barrières n’existent plus, voilà l’avantage sur lequel s’appuie The French Hub. De la sorte, il alimente la notion d’interconnectivité et de transmission entre de jeunes entrepreneurs à travers le monde.
+					</div>
+
+				</div>
+
+			</div>
+
+
+		</div>
+
+		<div id="conceptdown">
+
+				<div id="man"></div>
+
+				<div id="mac"></div>
+
+				<div id="conceptexplain2">Grâce à nos partenariats avec des fonds et des entreprises qualifiées, nous mettons les entrepreneurs dans les meilleures dispositions pour réussir leurs projets.
+					L’environnement de travail est primordial, il est impératif de baigner dans un environnement dynamique et rassurant pour réussir dans les meilleures conditions.
+				</div>
+
+				<div id="mer"></div>
+
+		</div>
+
+
+</div>
+
 	<!-- Footer -->
 	<?php include_once 'app/view/layout/footer_front.inc.php'; ?>
