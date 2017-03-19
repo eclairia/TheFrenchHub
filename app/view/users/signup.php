63ca105b3a3a9e764bd8c4eb45f391f650468338
Guillaume <guillaumehulin93@gmail.com>
2017-03-19 14:54:09 +0100

    Ajout du champ pour l'avatar

--- app/view/users/signup.php
+++ app/view/users/signup.php
@@ -1,62 +1,59 @@
 <?php include_once 'app/view/layout/header_front.inc.php'; ?>
 
 <div id="contentinscription">
+    <div id="forminscription">
+        <div id="titresignup"><h1 id="titleinscription">inscription</h1></div>
 
-  <div id="forminscription">
+        <form id="formsignup" method="post" action="?module=users&action=signup">
+            <div id="civility" class="formwidth">
+                <div><p id="pcivil">Civilité</p></div>
+                <div><input type="radio" id="radio1" name="user_civility" value="M"> M</div>
+                <div><input type="radio" id="radio2" name="user_civility" value="Mme"> Mme</div>
+                <div><input type="radio" id="radio3" name="user_civility" value="Mlle"> Mlle</div>
+            </div>
 
-    <div id="titresignup"><h1 id="titleinscription">inscription</h1></div>
+            <div>
+                <input class="formwidth borderform" name="user_last_name" type="text" placeholder="Nom" required>
+            </div>
 
-    <form id="formsignup" method="post" action="#">
+            <div>
+                <input class="formwidth borderform" name="user_first_name" type="text" placeholder="Prénom" required>
+            </div>
 
-      <div id="civility" class="formwidth">
+            <div>
+                <input class="formwidth borderform" name="user_birth" type="date" min="1940/01/01" placeholder="Date de naissance" required>
+            </div>
 
-        <div><p id="pcivil">Civilité</p></div>
-        <div><input type="radio" id="radio1" name="user_civility" value="M"> M</div>
-        <div><input type="radio" id="radio2" name="user_civility" value="Mme"> Mme</div>
-        <div><input type="radio" id="radio3" name="user_civility" value="Mlle"> Mlle</div>
+            <div>
+                <input class="formwidth borderform" name="user_mail" type="email" placeholder="Adresse mail" required>
+            </div>
 
-      </div>
+            <div>
+                <input class="formwidth borderform" name="user_login" type="text" placeholder="Identifiant" required>
+            </div>
 
-      <div>
-        <input class="formwidth borderform" name="user_last_name" type="text" placeholder="Nom" required></input>
+            <div>
+                <input class="formwidth borderform" name="user_password" type="password" placeholder="Mot de passe" required>
+            </div>
 
-      </div>
+            <div>
+                <input class="formwidth borderform" name="user_password2" type="password" placeholder="Confirmation mot de passe" required>
+            </div>
 
-      <div>
-        <input class="formwidth borderform" name="user_first_name" type="text" placeholder="Prénom" required></input>
-      </div>
+            <div>
+                <input type="file" class="formwidth borderform" name="user_avatar" placeholder="Avatar">
+            </div>
 
-      <div>
-        <input class="formwidth borderform" name="user_birth" type="date" min="1940/01/01" placeholder="Date de naissance" required></input>
-      </div>
+            <div>
+                <input type="hidden" name="user_level" value="0">
+                <input class="formwidth boutonform" type="submit" placeholder="Inscription">
+            </div>
 
-      <div>
-        <input class="formwidth borderform" name="user_mail" type="email" placeholder="Adresse mail" required></input>
-      </div>
-
-      <div>
-        <input class="formwidth borderform" name="user_login" type="text" placeholder="Identifiant" required></input>
-      </div>
-
-      <div>
-        <input class="formwidth borderform" name="user_password" type="password" placeholder="Mot de passe" required></input>
-      </div>
-
-      <div>
-        <input class="formwidth borderform" name="user_password2" type="password" placeholder="Confirmation mot de passe" required></input>
-      </div>
-
-      <div>
-        <input class="formwidth boutonform" type="submit" placeholder="Inscription"></input>
-      </div>
-
-      <div>
-        <input class="formwidth boutonform" type="reset" placeholder="Réinitialiser"></input>
-      </div>
-
-    </form>
-
-  </div>
+            <div>
+                <input class="formwidth boutonform" type="reset" placeholder="Réinitialiser">
+            </div>
+        </form>
+    </div>
 
 </div>
 <?php include_once 'app/view/layout/footer_front.inc.php'; ?>
