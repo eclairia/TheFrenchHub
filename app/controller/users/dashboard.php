4a4047deaef249008fc8490f9994c80ef226fd7f
Guillaume <guillaumehulin93@gmail.com>
2017-03-19 18:18:57 +0100

    Début du dashboard en dynamique

--- app/controller/users/dashboard.php
+++ app/controller/users/dashboard.php
@@ -1,27 +1,28 @@
 <?php
-	//Faire select_table users pour récupérer les membres d'un project
-	define("PAGE_TITLE", "Votre espace utilisateur The French Hub");
-	include_once("app/view/users/dashboard.php");
+    //protection("user", "users", "login", USER_LAMBDA);
 
-	//$_GET['id'] = intval($_GET['id']);
-	/*$users_project =  select_table(
+    $_SESSION['user']['user_project'] = $_SESSION['user']['user_project'] == null ? 0 : $_SESSION['user']['user_project'];
+
+	$user_project = select_table_value(
         		array("table1" => "tfh_users",
-        				"table2" => "tfh_projects"),
-        		array(
-        			"where_colum" => "project_ID",
-        			"where_value" => $_GET['id']));*/
+                      "table2" => "tfh_projects"),
+        		array("where_column" => "project_ID",
+                      "where_value" => $_SESSION['user']['user_project']),
+                array("and_column1" => "user_login",
+                      "and_value1" => $_SESSION['user']['user_login'],
+                      "and_column2" => "user_password",
+                      "and_value2" => $_SESSION['user']['user_password']));
 
-	//var_dump($users_project);
-	//die();
+    $nb_additionnal_members = count_table(array("table1" => "tfh_additionnal_members"),
+                                          array("where_column" => "additionnal_member_project",
+                                                "where_value" => $_SESSION['user']['user_project']));
 
-	// var_dump($users_project);
-	// die();
+    $order = select_table_value(
+        array("table1" => "tfh_orders"),
+        array("where_column" => "order_user_password",
+              "where_value" => $_SESSION['user']['user_password']),
+        array("and_column1" => "order_user_login",
+              "and_value1" => $_SESSION['user']['user_login']));
 
-	// $project_service =  select_table(
- 	//        		array("table1" => "tfh_users",
- 	//        				"table2" => "tfh_projects"),
- 	//        		array(
- 	//        			"where_colum => project_ID",
- 	//        			"where_value => user_project"));
-
+	define("PAGE_TITLE", "Votre espace utilisateur The French Hub");
 	include_once("app/view/users/dashboard.php");
--- app/view/users/dashboard.php
+++ app/view/users/dashboard.php
@@ -18,21 +18,20 @@
                 <p>Projet validé</p>
                 <p>Membres ajoutés</p>
                 <p>Offre choisie</p>
-            
-        </div>
+            </div>
 
-        <div id="checkedariane">
-            <div class="checked"></div>
-            <div class="unchecked"></div>
-            <div class="unchecked"></div>
-            <div class="unchecked"></div>
-            <div class="unchecked"></div>
-        </div>
+            <div id="checkedariane">
+                <div class="checked"></div>
+                <div class="<?php echo ($_SESSION['user']['user_project'] != null ? 'checked' : 'unchecked')?>"></div>
+                <div class="<?php echo (!empty($user_project) ? ($user_project[0]['project_confirmed'] == 1 ? 'checked' : 'unchecked') : "unchecked" )?>"></div>
+                <div class="<?php echo ($nb_additionnal_members > 0 ? 'checked' : 'unchecked')?>"></div>
+                <div class="<?php echo (!empty($order) ? 'checked' : 'unchecked')?>"></div>
+            </div>
 
-        <div id="hrariane"></div>
+            <div id="hrariane"></div>
+        </div>
     </div>
 </div>
-    </div>
 
 <div id="allcontentdashboardbot">
     <div id="dashleft">
@@ -42,10 +41,10 @@
                     <span>(Vous ne pouvez ajouter des membres qu'une fois le projet validé.)</span>
                 </h2>
 
-                <input class="formwidthdash2 borderform" type="text" placeholder="Nom" name="additionnal_member_last_name"></input>
-                <input class="formwidthdash2 borderform" type="text" placeholder="Prénom" name="additionnal_member_first_name"></input>
-                <input class="formwidthdash2 borderform" type="email" placeholder="Adresse mail" name="additionnal_member_mail"></input>
-                <input class="boutonform3" type="submit" value="Ajouter un membre"></input>
+                <input class="formwidthdash2 borderform" type="text" placeholder="Nom" name="additionnal_member_last_name">
+                <input class="formwidthdash2 borderform" type="text" placeholder="Prénom" name="additionnal_member_first_name">
+                <input class="formwidthdash2 borderform" type="email" placeholder="Adresse mail" name="additionnal_member_mail">
+                <input class="boutonform3" type="submit" value="Ajouter un membre">
             </div>
         </form>
     </div>
@@ -54,10 +53,10 @@
         <form method="post" action="">
             <div class="formaddmember2">
             <h2 class="titlechoice2">Votre projet</h2>
-            <input class="formwidthdash2 borderform" type="text" placeholder="Nom de votre projet" name="project_name"></input>
+            <input class="formwidthdash2 borderform" type="text" placeholder="Nom de votre projet" name="project_name">
             <textarea class="formwidthdash borderformdash" placeholder="Décrivez votre projet en quelques mots" name="project_description"></textarea>
-            <input class="formwidthdash2 borderform" type="file" placeholder="Fichier à joindre" name="project_file"></input>
-            <input class="boutonform2" type="submit" value="Soumettre votre projet"></input>
+            <input class="formwidthdash2 borderform" type="file" name="project_file">
+            <input class="boutonform2" type="submit" value="Soumettre votre projet">
             </div>
         </form>
 
