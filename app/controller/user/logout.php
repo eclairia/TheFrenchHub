4f25b8fe29bbb444871b27589b4a0bc9aa156dbf
Guillaume <guillaumehulin93@gmail.com>
2017-03-21 00:59:02 +0100

    nettoyage des fichiers

--- app/controller/user/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	session_destroy();
-	session_unset();
-	unset($_SESSION["user"]);
-
-	header("Location:?module=users&action=login");
-	exit;
--- app/controller/users/test_uri.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$url = "http://". $_SERVER['SERVER_NAME'] ."". substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/', 20)+1) ."?module=users&action=return";
-echo 'Server Name= ' . $_SERVER['SERVER_NAME'] . '<br />';
-echo '$uri= ' . $_SERVER['REQUEST_URI'] . '<br />';
-echo '$url= ' . $url . '<br />';
-echo 'strripos return = ' . strripos($_SERVER['REQUEST_URI'], '/', -5) . '<br />';
