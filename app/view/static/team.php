67ccd45c6a641d1d55a978f2963d487044018ddb
Romain Rouleau <Romain Rouleau>
2017-03-22 13:00:10 +0100

    modifs en tout genre biatch

--- app/view/static/team.php
+++ app/view/static/team.php
@@ -13,56 +13,94 @@
 						</div>
 
 					</div>
-
 			</div>
 
-			<div id="membres">
+			<div id="allteam">
 
-				<div id="teamhaut">
+				<div class="companyteam">
 
-					<div id="amel" class="divmember">
-						<div class="teamdisplaynone">
-							<p class="textnone">Amel Guellal <br/><span class="poles">Pôle Marketing</span></p>
-						</div>
-					</div>
-
-					<div id="guillaume" class="divmember">
-						 <div class="teamdisplaynone">
-							 <p class="textnone">Guillaume Hulin <br/><span class="poles">Pôle Développement</span></p>
-						 </div>
-					 </div>
-
-					<div id="carla" class="divmember">
-						 <div class="teamdisplaynone">
-							 <p class="textnone">Carla Succar <br/><span class="poles">Pôle Webdesign</span></p>
-						 </div>
-					 </div>
+					<div id="amel"></div>
+					<div><p class="textcompany"><span>AMEL : Fondatrice de The French Hub et spécialisé dans le webmarketing.</span></br></br>
+						 Amel a toujours été attiré par les innovations et les nouvelles technologies.</br></br>
+								“Chaque bonne réalisation, grande ou petite, connait ses périodes de corvées et de triomphes; un début, un combat et une victoire.” (Mahatma Gandhi).</br></br>
+								Selon Amel, l'échec n’existe pas il y’a toujours une leçon derrière pour mieux rebondir ensuite. La vie est ainsi faite, avec elle tu apprendras à toujours persévérer car c’est en persévérant et en gardant la tête haute qu’on réussit.
+								</p></div>
 
 				</div>
 
-				<div id="teambas">
+				<div class="companyteam2">
 
-					<div id="louis" class="divmember">
-						<div class="teamdisplaynone">
-							<p class="textnone">Louis FIessinger <br/><span class="poles">Pôle Marketing</span></p>
-						</div>
-					</div>
+					<div id="guillaume"></div>
+					<div><p class="textcompany2"><span>GUILLAUME : Co-fondateur de The French Hub, Guillaume intervient en développement et en intégration.</span></br></br>
+						 Sa passion pour les technologies du digitale le pousse à innover constamment.</br></br>
+								“S’il n’y a pas de solutions c’est qu’il n’y a pas de problèmes !”</br></br>
+								</p></div>
 
-					<div id="adrien" class="divmember">
-						<div class="teamdisplaynone">
-							<p class="textnone">Adrien Jourdier <br/><span class="poles">Pôle Développement</span></p>
-						</div>
-				 </div>
+				</div>
 
-					<div id="romain" class="divmember">
-						<div class="teamdisplaynone">
-							<p class="textnone">Romain Rouleau <br/><span class="poles">Pôle Webdesign</span></p>
-						</div>
-					</div>
+				<div class="companyteam">
+
+					<div id="carla"></div>
+					<div><p class="textcompany"><span>CARLA : Fondatrice de The French Hub, spécialisée dans le design digitale, le marketing et la
+						communication.</span></br></br>
+						 Carla est passionnée par l’innovation et les challenges.</br></br>
+						 "Nous n’avons pas toujours la chance de faire beaucoup de choses, et c’est pourquoi
+
+						 elles doivent être excellente. Il faut que nous marquions le monde, que nous apportions un
+
+						 changement. C’est l’innovation qui fait la différence entre un leader et un suiveur, alors
+
+						 innovez !" (Carla Succar)</br></br>
+						 Carla est enthousiaste et adore aider et accompagner des projets innovants. Si elle te
+
+						 choisit dans son équipe, tu travailleras dans la bonne humeur.
+								</p></div>
+
+				</div>
+
+				<div class="companyteam2">
+
+					<div id="louis"></div>
+					<div><p class="textcompany2"><span>LOUIS : Fondateur de The French Hub, spécialisé dans le marketing numérique et la
+								communication</span>
+								</br></br>
+								Louis est passionné par l’innovation et les nouvelles technologies.
+						  	</br></br>
+								"L’économie de demain passe par la création d’un univers d’entrepreneurs connectés et
+
+									solidaires . En tant que français je suis fière de pouvoir être à l’origine d’une écosystème
+
+									internationale de startup gravitant autour de la France." (Louis Fiessinger)
+								</br></br>
+								Louis il est optimiste et il adore se creuser la tête pour trouver de bonnes solutions. Si il te
+								choisit dans son équipe, tu retrouveras rapidement le sourire.
+								</p></div>
+
+				</div>
+
+				<div class="companyteam">
+
+					<div id="adrien"></div>
+					<div><p class="textcompany"><span>ADRIEN : Co-fondateur de The French Hub et spécialisé dans le développement.</span></br></br>
+						 Adrien a toujours été attiré par les innovations et les nouvelles technologies et bien sûr le beau code.</br></br>
+								“Il y a une différence entre connaître le chemin, et arpenter le chemin” (Andy Washowski).</br></br>
+								</p></div>
+
+				</div>
+
+				<div class="companyteam2">
+
+					<div id="romain"></div>
+					<div><p class="textcompany2"><span>ROMAIN : Fondateur de The French Hub spécialisé dans le design, l'UX et l'intégration.</span></br></br>
+						 Romain est passionné par l'univers du design et du jeu vidéo.</br></br>
+								“” (Kaaris).</br></br>
+
+								</p></div>
 
 				</div>
 
 			</div>
+
 
 	</div>
 
