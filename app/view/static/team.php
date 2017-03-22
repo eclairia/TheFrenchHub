81c5501d01cd7826e86d863129e9dcf1df73f1ba
Romain Rouleau <Romain Rouleau>
2017-03-22 14:39:54 +0100

    page équipe desktop + responsive

--- app/view/static/team.php
+++ app/view/static/team.php
@@ -19,9 +19,26 @@
 
 				<div class="companyteam">
 
-					<div id="amel"></div>
-					<div><p class="textcompany"><span>AMEL : Fondatrice de The French Hub et spécialisé dans le webmarketing.</span></br></br>
-						 Amel a toujours été attiré par les innovations et les nouvelles technologies.</br></br>
+					<div id="amel">
+						<div class="noneteamhover">
+
+							<div>
+								<p class="titleteamhover">Elle aime</p><br/>
+								<p>Voyager</p><br/>
+								<p>Le couscous de maman</p><br/>
+								<p>Le Nokia 3310</p><br/>
+							</div>
+							<div>
+								<p class="titleteamhover">Elle déteste</p><br/>
+								<p>Les gens pas motivés</p><br/>
+								<p>Le café</p><br/>
+								<p>Les bugs internet</p><br/>
+							</div>
+
+						</div>
+					</div>
+					<div><p class="textcompany"><span>AMEL : Fondatrice de The French Hub et spécialisé dans le webmarketing.</span><br/><br/>
+						 Amel a toujours été attiré par les innovations et les nouvelles technologies.<br/><br/>
 								“Chaque bonne réalisation, grande ou petite, connait ses périodes de corvées et de triomphes; un début, un combat et une victoire.” (Mahatma Gandhi).</br></br>
 								Selon Amel, l'échec n’existe pas il y’a toujours une leçon derrière pour mieux rebondir ensuite. La vie est ainsi faite, avec elle tu apprendras à toujours persévérer car c’est en persévérant et en gardant la tête haute qu’on réussit.
 								</p></div>
@@ -30,27 +47,61 @@
 
 				<div class="companyteam2">
 
-					<div id="guillaume"></div>
-					<div><p class="textcompany2"><span>GUILLAUME : Co-fondateur de The French Hub, Guillaume intervient en développement et en intégration.</span></br></br>
-						 Sa passion pour les technologies du digitale le pousse à innover constamment.</br></br>
-								“S’il n’y a pas de solutions c’est qu’il n’y a pas de problèmes !”</br></br>
+					<div id="guillaume">
+						<div class="noneteamhover">
+
+							<div>
+								<p class="titleteamhover">Il aime</p><br/>
+								<p>Les jeux vidéo</p><br/>
+								<p>Le 280</p><br/>
+								<p>La mousse au chocolat</p><br/>
+							</div>
+							<div>
+								<p class="titleteamhover">Il déteste</p><br/>
+								<p>Dessiner</p><br/>
+								<p>Apple</p><br/>
+								<p>Les politiques</p><br/>
+							</div>
+
+						</div>
+					</div>
+					<div><p class="textcompany2"><span>GUILLAUME : Co-fondateur de The French Hub, Guillaume intervient en développement et en intégration.</span><br/><br/>
+						 Sa passion pour les technologies du digitale le pousse à innover constamment.<br/><br/>
+								“S’il n’y a pas de solutions c’est qu’il n’y a pas de problèmes !”<br/><br/>
 								</p></div>
 
 				</div>
 
 				<div class="companyteam">
 
-					<div id="carla"></div>
+					<div id="carla">
+						<div class="noneteamhover">
+
+							<div>
+								<p class="titleteamhover">Elle aime</p><br/>
+								<p>Rigoler</p><br/>
+								<p>Les désserts</p><br/>
+								<p>La bonne musique</p><br/>
+							</div>
+							<div>
+								<p class="titleteamhover">Elle déteste</p><br/>
+								<p>La mauvaise humeur</p><br/>
+								<p>Perdre un duel</p><br/>
+								<p>Les épinards</p><br/>
+							</div>
+
+						</div>
+					</div>
 					<div><p class="textcompany"><span>CARLA : Fondatrice de The French Hub, spécialisée dans le design digitale, le marketing et la
-						communication.</span></br></br>
-						 Carla est passionnée par l’innovation et les challenges.</br></br>
+						communication.</span><br/><br/>
+						 Carla est passionnée par l’innovation et les challenges.<br/><br/>
 						 "Nous n’avons pas toujours la chance de faire beaucoup de choses, et c’est pourquoi
 
 						 elles doivent être excellente. Il faut que nous marquions le monde, que nous apportions un
 
 						 changement. C’est l’innovation qui fait la différence entre un leader et un suiveur, alors
 
-						 innovez !" (Carla Succar)</br></br>
+						 innovez !" (Carla Succar)<br/><br/>
 						 Carla est enthousiaste et adore aider et accompagner des projets innovants. Si elle te
 
 						 choisit dans son équipe, tu travailleras dans la bonne humeur.
@@ -60,18 +111,35 @@
 
 				<div class="companyteam2">
 
-					<div id="louis"></div>
+					<div id="louis">
+						<div class="noneteamhover">
+
+							<div>
+								<p class="titleteamhover">Il aime</p><br/>
+								<p>Les gens souriants</p><br/>
+								<p>Les entrecôtes</p><br/>
+								<p>Les draps qui sentent bon</p><br/>
+							</div>
+							<div>
+								<p class="titleteamhover">Il déteste</p><br/>
+								<p>Les gens qui abandonnent</p><br/>
+								<p>Les araignées</p><br/>
+								<p>Les choux de bruxelles</p><br/>
+							</div>
+
+						</div>
+					</div>
 					<div><p class="textcompany2"><span>LOUIS : Fondateur de The French Hub, spécialisé dans le marketing numérique et la
 								communication</span>
-								</br></br>
+							<br/><br/>
 								Louis est passionné par l’innovation et les nouvelles technologies.
-						  	</br></br>
+						  	<br/><br/>
 								"L’économie de demain passe par la création d’un univers d’entrepreneurs connectés et
 
 									solidaires . En tant que français je suis fière de pouvoir être à l’origine d’une écosystème
 
 									internationale de startup gravitant autour de la France." (Louis Fiessinger)
-								</br></br>
+								<br/><br/>
 								Louis il est optimiste et il adore se creuser la tête pour trouver de bonnes solutions. Si il te
 								choisit dans son équipe, tu retrouveras rapidement le sourire.
 								</p></div>
@@ -80,21 +148,54 @@
 
 				<div class="companyteam">
 
-					<div id="adrien"></div>
-					<div><p class="textcompany"><span>ADRIEN : Co-fondateur de The French Hub et spécialisé dans le développement.</span></br></br>
-						 Adrien a toujours été attiré par les innovations et les nouvelles technologies et bien sûr le beau code.</br></br>
-								“Il y a une différence entre connaître le chemin, et arpenter le chemin” (Andy Washowski).</br></br>
+					<div id="adrien">
+						<div class="noneteamhover">
+							<div>
+								<p class="titleteamhover">Il aime</p><br/>
+								<p>Voyager</p><br/>
+								<p>Le Badminton</p><br/>
+								<p>Les Git Push</p><br/>
+							</div>
+							<div>
+								<p class="titleteamhover">Il déteste</p><br/>
+								<p>Être original</p><br/>
+								<p>Le café</p><br/>
+								<p>Les erreurs PHP</p><br/>
+							</div>
+						</div>
+				, </div>
+					<div><p class="textcompany"><span>ADRIEN : Co-fondateur de The French Hub et spécialisé dans le développement.</span>
+						<br/></br>
+						 Adrien a toujours été attiré par les innovations et les nouvelles technologies et bien sûr le beau code.<br/><br/>
+								“Il y a une différence entre connaître le chemin, et arpenter le chemin” (Andy Washowski).<br/><br/>
 								</p></div>
 
 				</div>
 
-				<div class="companyteam2">
+				<div class="companyteam2 basteamspace">
 
-					<div id="romain"></div>
-					<div><p class="textcompany2"><span>ROMAIN : Fondateur de The French Hub spécialisé dans le design, l'UX et l'intégration.</span></br></br>
-						 Romain est passionné par l'univers du design et du jeu vidéo.</br></br>
-								“” (Kaaris).</br></br>
+					<div id="romain">
+						<div class="noneteamhover">
 
+							<div>
+								<p class="titleteamhover">Il aime</p><br/>
+								<p>Le Tiramisu</p><br/>
+								<p>Les singes</p><br/>
+								<p>Le Lorem Ipsum</p><br/>
+							</div>
+							<div>
+								<p class="titleteamhover">Il déteste</p><br/>
+								<p>Les lundi matins</p><br/>
+								<p>Les endives</p><br/>
+								<p>La flemmardise</p><br/>
+							</div>
+
+						</div>
+					</div>
+					<div><p class="textcompany2"><span>ROMAIN : Fondateur de The French Hub spécialisé dans le design, l'UX et l'intégration.</span><br/><br/>
+						 Romain est passionné par l'univers du design et du jeu vidéo.<br/><br/>
+								"Il faut que tu crois encore plus ce que tu crois, et quand tu commences à croire ce que tu crois, y a personne au monde qui peut te bouger !" (JC VD).<br/><br/>
+								Romain est d'un naturel positif et persévérant, c'est pour ainsi dire un "Try hard" qui ne s'arrête de travailler que lorsqu'il est satisfait de son travail.
 								</p></div>
 
 				</div>
